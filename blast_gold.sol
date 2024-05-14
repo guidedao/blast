20fca207c867778c3a8a4923b159ad38d309c67d
Guide DAO <yo@moscoding.ru>
2024-05-15 02:43:44 +0300

    Create blast_gold.sol

---
+++ blast_gold.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.16 <0.9.0;
+
+interface IBlastPoints {
+  function configurePointsOperator(address operator) external;
+    function configurePointsOperatorOnBehalf(address contractAddress, address operator) external;
+}
+
+contract GuideDAO {
+  construtor(address _pointsOperator) {
+    IBlastPoints(0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800).configurePointsOperator(_pointsOperator);
+  }
+}
